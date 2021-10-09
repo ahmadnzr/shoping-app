83c2dadf7cec02ffd4301dfbc61593538ae6d6cc
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-10-10 00:33:05 +0700

    success page after checkout

--- shop-client-ui/src/pages/Success.jsx
+++ shop-client-ui/src/pages/Success.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { useLocation } from 'react-router'
+import React from "react";
+import { useLocation } from "react-router";
 
 function Success() {
-    const location = useLocation()
-    console.log(location);
-    return (
-        <div>
-            successful
-        </div>
-    )
+  const location = useLocation();
+  const data = location.state.data;
+
+  return (
+    <div>
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Successfull. Your order is being prepared...
+        <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
+      </div>
+    </div>
+  );
 }
 
-export default Success
+export default Success;
