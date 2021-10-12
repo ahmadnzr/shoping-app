f3c72b96a837e195db65c4d7df552ca7d78bf874
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-10-12 16:53:30 +0700

    get token from local storage

--- shop-client-ui/src/requestMethod.js
+++ shop-client-ui/src/requestMethod.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNGNhMzc0ODBlNTZiYjBkZjI1MzBmYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzY4MjYyMiwiZXhwIjoxNjMzOTQxODIyfQ.vQbTn43qc31LgNibhKX6xgKMMEoD4Zw3CPJx_10hy48";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+.currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
