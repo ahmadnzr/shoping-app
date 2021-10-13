94b977cb5a00c2a209337906dfa15bdcda770695
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-10-13 14:26:30 +0700

    update income route

--- shop-api/routes/order.js
+++ shop-api/routes/order.js
@@ -66,13 +66,21 @@ router.get("/", verifyAdmin, async (req, res) => {
 // get monthly income
 
 router.get("/income", verifyAdmin, async (req, res) => {
+  const productId = req.query.pid;
   const date = new Date();
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
   try {
     const income = await Order.aggregate([
-      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
+          ...(productId && {
+            products: { $elemMatch: { productId } },
+          }),
+        },
+      },
       {
         $project: {
           month: { $month: "$createdAt" },
@@ -91,5 +99,4 @@ router.get("/income", verifyAdmin, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 module.exports = router;
