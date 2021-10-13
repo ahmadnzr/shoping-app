9e2fcfa57b94263f4afc419a7733cedfc9e0a637
Ahmad Nizar <ahmad.rpl001@gmail.com>
2021-10-13 14:26:03 +0700

    CRUD operation for products

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVjRmEtsemsxWkxwxqk-jeKLdOFg_1C0w",
+  authDomain: "shop-d45d8.firebaseapp.com",
+  projectId: "shop-d45d8",
+  storageBucket: "shop-d45d8.appspot.com",
+  messagingSenderId: "531104012724",
+  appId: "1:531104012724:web:3258336a90266254467ae1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
